f7b9959b8aa8f8c19c26f69691aad6a9b6f7a3d1
hanuman <a.kolyan@gratun.am>
2023-03-09 14:11:16 +0400

    setup and List ADT

---
+++ src/03 Lists/lists.spec.ts
@@ -0,0 +1,66 @@
+import { describe, expect, test } from '@jest/globals';
+import { TList, ListFactory } from './lists';
+
+describe('List ADT tests, simple values', () => {
+    let list: TList = ListFactory()
+    beforeEach(() => {
+        list.clear()
+        list.append("Sun")
+        list.append("Moon")
+        list.append("Jupiter")
+        list.append("Saturn")
+    })
+
+
+    test('clear', () => {
+        list.clear()
+        expect(list.length()).toBe(0)
+    })
+
+    test('find 3 elements', () => {
+        expect(list.find("Sun")).toBe(0)
+        expect(list.find("Moon")).toBe(1)
+        expect(list.find("Jupiter")).toBe(2)
+    })
+
+    test("insert element", () => {
+        list.insert("Venera", "Moon")
+        expect(list.length()).toBe(5)
+        expect(list.find("Venera")).toBe(2)
+    })
+
+    test('4 elements + append one', () => {
+        expect(list.length()).toBe(4)
+        list.append("Venera")
+        expect(list.length()).toBe(5)
+    });
+
+    test('remove element', () => {
+        list.remove("Moon")
+        expect(list.length()).toBe(3)
+        expect(list.find("Moon")).toBe(-1)
+    })
+
+    test('front & next', () => {
+        list.front()
+        expect(list.currPos()).toBe(0)
+        expect(list.next()).toBe(1)
+    })
+
+    test('end & prev', () => {
+        list.end()
+        expect(list.currPos()).toBe(3)
+        expect(list.prev()).toBe(2)
+    })
+
+    test('move to position', () => {
+        list.moveTo(1)
+        expect(list.currPos()).toBe(1)
+        expect(list.getElement()).toBe("Moon")
+    })
+
+    test('lenght', () => {
+        expect(list.length()).toBe(4)
+    })
+
+});
---
+++ src/03 Lists/lists.ts
@@ -0,0 +1,96 @@
+export type TList = {
+    clear: () => void
+    find: (element: any) => number
+    toString: () => string
+    insert: (element: any, after: any) => boolean
+    append: (element: any) => void
+    remove: (element: any) => boolean
+    front: () => void
+    end: () => void
+    prev: () => void
+    next: () => void
+    currPos: () => number
+    moveTo: (position: number) => void
+    getElement: () => any
+    length: () => number
+}
+
+export const ListFactory = (): TList => {
+    let listSize = 0;
+    let pos = 0;
+    let dataStore: any[] = [];
+
+    const clear = () => {
+        dataStore = [];
+        listSize = pos = 0;
+    };
+
+    
+    const find = (element: any) => {
+        for (let i = 0; i < dataStore.length; i++) {
+            if (dataStore[i] === element)
+                return i;
+        }
+        return -1;
+    };
+
+    const toString = () => dataStore.toString();
+
+    const insert = (element: any, after: any) => {
+        const insertPos = find(after);
+
+        if (insertPos < 0)
+            return false
+
+        dataStore.splice(insertPos + 1, 0, element);
+        ++listSize;
+        return true;
+    };
+
+    const append = (element: any) => {
+        dataStore[listSize++] = element;
+    };
+
+    const remove = (element: any) => {
+        const foundAt = find(element);
+        if (foundAt > -1) {
+            dataStore.splice(foundAt, 1);
+            --listSize;
+            return true;
+        }
+        return false;
+    };
+
+    const front = () => (pos = 0);
+
+    const end = () => (pos = listSize - 1);
+
+    const prev = () => (pos > 0) && --pos;
+
+    const next = () => (pos < listSize - 1) && ++pos;
+
+    const currPos = () => pos;
+
+    const moveTo = (position: number) => pos = position;
+
+    const getElement = () => dataStore[pos];
+
+    const length = () => listSize;
+    
+    return {
+        clear,
+        find,
+        toString,
+        insert,
+        append,
+        remove,
+        front,
+        end,
+        prev,
+        next,
+        currPos,
+        moveTo,
+        getElement,
+        length
+    }
+}
